9583247e8cdf577ac2b016f23ae9c5f2a32706ce
sebika <sebymarinescu@yahoo.ca>
2020-07-08 17:20:56 +0300

    Pagination and bugs solved

--- source/blog/models.py
+++ source/blog/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title
+        return self.title + ' ' + self.author.username + ' ' + str(self.author_id)
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk':self.pk})
--- source/users/models.py
+++ source/users/models.py
@@ -8,10 +8,10 @@ class Profile(models.Model):
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return f'{self.user.username}\'s Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
         img = Image.open(self.image.path)
 
